6c8d01d6c2f61d6f45098e72f627154f402c3760
Victor Ramirez <victorriver6@hotmail.com>
2024-03-13 19:49:04 -0300

    Detectar sec_escape y quitarlas al mostrarlas (ej adicional), tambien se muestran tramas con longitud y checksum incorrecto

--- tp1.py
+++ tp1.py
@@ -1,3 +1,45 @@
+def separarTramas(contenido):
+    tramas = []
+    trama_actual = ""
+    carga_util = 0
+    count = 0 # Sirve para contar la trama en la que estoy
+    pos_sec_escape = []
+    for i in range(0,len(contenido),2):
+
+        byte = contenido[i:i+2]
+
+        if byte != "7E":
+            trama_actual = trama_actual + byte
+        else:
+            if i==0:
+                trama_actual = trama_actual + byte
+                continue
+            if contenido[i-2:i] == "7D":
+                carga_util += 1
+                trama_actual = trama_actual + byte
+                pos_sec_escape.append(count)
+            else:
+                tramas.append(trama_actual)
+                count += 1
+                trama_actual = "7E"
+
+    mostrar_sec_escape(tramas,pos_sec_escape)
+    return tramas,carga_util
+
+def mostrar_sec_escape(tramas,posiciones):
+    for i in posiciones:
+        nueva_trama = quitar_sec_escape(tramas[i])
+        print(f"La trama {i} tiene secuencia de escape")
+        print(f"La trama {i} SIN secuencia de escape es: {nueva_trama}")
+def quitar_sec_escape(trama):
+    nueva_trama = ""
+    for i in range(0,len(trama),2):
+        if trama[i:i+2] == "7D" and trama[i+2:i+4] == "7E":
+            continue
+        else:
+            nueva_trama = nueva_trama + trama[i:i+2]
+    return nueva_trama
+
 def comprobarLongitudes(tramas):
     longitud_1 = []
     longitud_2 = []
@@ -12,13 +54,18 @@ def comprobarLongitudes(tramas):
     
     correctas = 0
     incorrectas = 0
+    tramas_correctas = []
+    count = 0
     for i in range(len(longitud_1)):
         if longitud_1[i] == longitud_2[i]:
             correctas += 1
+            tramas_correctas.append(tramas[i])
         else:
             incorrectas += 1
+            print(f"Trama {count} tiene longitud incorrecta: {tramas[count]}")
+        count += 1
 
-    return correctas,incorrectas
+    return correctas,incorrectas,tramas_correctas
 
 def sumarBytes(cadena):
     sum = 0
@@ -42,47 +89,37 @@ def verificar_checkSum(tramas):
     num_hex = int("0xFF",16) 
     correctos = 0
     incorrectos = 0
+    count = 0
     for i in range(len(check_sum_array)):
         checkSum = num_hex - (sum_array[i] & num_hex) #Formula del trabajo practico
         if checkSum == check_sum_array[i]: 
             correctos += 1
         else:
             incorrectos += 1
+            print(f"Trama {count} tiene checksum incorrecto: {tramas[count]}")
+        count += 1
 
     return correctos,incorrectos
 
 archivo = open("Tramas_802-15-4.log","r")
 contenido = archivo.read()
+aux = separarTramas(contenido) #Guarda las tramas y la cantidad de tramas con carga util
+tramas = aux[0]
 
-tramas = []
-pos_inicial = contenido.find("7E")
-longitud = len(contenido)
-new_pos = 0
-
-while (new_pos != -1):
-    resto_trama = contenido[pos_inicial+2:longitud]
-    # Busco el inicio de la otra trama, es decir un 7E
-    new_pos = resto_trama.find("7E")
-    if new_pos == -1: #Caso donde no queda otro 7E
-        trama_interior = contenido[pos_inicial:longitud]
-        tramas.append(trama_interior)
-    else:
-        """new_pos esta desplazado 2 posiciones adelante con respecto a contenido
-        ya que su posicion fue calculada respecto a resto_trama"""
-        trama_interior = contenido[pos_inicial:new_pos+2] 
-        tramas.append(trama_interior) #Agrego toda la trama incluyendo el 7E
-        contenido = contenido[new_pos+2:longitud] # Ahora mi contenido va desde el siguiente 7E hasta el final
-        pos_inicial = 0 # Reseteo la posicion 
-
-
-
-
-
-print(len(tramas))
 respuestas = comprobarLongitudes(tramas)
+tramas_correctas = respuestas[2]
+respuestas_con_checksum = verificar_checkSum(tramas_correctas)
+
+
+print(f"Numero total de tramas: {len(tramas)}")
 print(f"Cantidad de longitudes correctas: {respuestas[0]}")
 print(f"Cantidad de longitudes incorrectas: {respuestas[1]}")
 
-respuestas_con_checksum = verificar_checkSum(tramas)
-print(f"Cantidad de longitudes correctas con verificacion: {respuestas_con_checksum[0]}")
-print(f"Cantidad de longitudes correctas sin verificacion: {respuestas_con_checksum[1]}")
+print(f"Tramas de longitud correcta con checksum correcto: {respuestas_con_checksum[0]}")
+print(f"Tramas de longitud correcta con checksum incorrecto: {respuestas_con_checksum[1]}")
+
+print(f"Números de tramas que utilizan secuencia de escape: {aux[1]}")
+
+"""#Falta corregir el el numero de trama para el checksum incorrecto,
+recordar que se hace respecto a las tramas totales y el checksum respecto a las
+tramas con longitud correcta"""
